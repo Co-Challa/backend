3e988fc7daebb3b75cab7dcc95d13a90959dac1f
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-17 08:13:35 +0900

    first commit and entity add

---
+++ src/main/java/com/cochalla/cochalla/entity/LikeId.java
@@ -0,0 +1,22 @@
+package com.cochalla.cochalla.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class LikeId implements Serializable {
+    private Integer post;
+    private String user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LikeId)) return false;
+        LikeId likeId = (LikeId) o;
+        return Objects.equals(post, likeId.post) && Objects.equals(user, likeId.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(post, user);
+    }
+}
