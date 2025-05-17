56bd4a2a63d5fffa56f52981356a1be1cff88bb1
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-17 14:22:10 +0900

    Create Signup API
    회원가입 API 구현 및 패스워드 암호화 적용

---
+++ src/main/java/com/cochalla/cochalla/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.cochalla.cochalla.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cochalla.cochalla.dto.LoginRequestDto;
+import com.cochalla.cochalla.dto.SignupRequestDto;
+import com.cochalla.cochalla.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    @Autowired
+    UserService user_service;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody SignupRequestDto requestDto) {
+        user_service.signup(requestDto);
+        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto requestDto) {
+        String token = user_service.login(requestDto);
+        return ResponseEntity.ok()
+        .header("Authorization", "Bearer " + token)
+        .body("로그인 성공");
+    }
+    
+    
+}
