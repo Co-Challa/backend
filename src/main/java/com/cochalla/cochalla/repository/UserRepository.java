0d8e3622c0cadc885943eca55d828eaee10a3810
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-17 14:36:30 +0900

    import 경로 변경
    엔티티 -> 도메인으로 인한 import 경로 변경

--- src/main/java/com/cochalla/cochalla/repository/UserRepository.java
+++ src/main/java/com/cochalla/cochalla/repository/UserRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cochalla.cochalla.entity.User;
+import com.cochalla.cochalla.domain.User;
 
 public interface UserRepository extends JpaRepository<User, String>{
     Optional<User> findByUserId(String userId);
