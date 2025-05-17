13704b0f4df05336920ffc5aa15b846c9c5116bb
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-17 14:24:06 +0900

    Login + Jwt
    로그인 기능 + JWT 생성 및 검증

---
+++ src/main/java/com/cochalla/cochalla/security/JwtUtil.java
@@ -0,0 +1,63 @@
+package com.cochalla.cochalla.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class JwtUtil {
+    private Key key;
+    private final long TOKEN_VALID_TIME = 60 * 60 * 1000L;
+
+    @PostConstruct
+    public void init(){
+        try{
+            Dotenv dotenv = Dotenv.configure()
+            .directory("./")
+            .load();
+            String envSecret = dotenv.get("JWT_SECRET_KEY");
+            key = Keys.hmacShaKeyFor(envSecret.getBytes());
+        } catch (Exception e){
+            throw new IllegalStateException("JWT 비밀키 로딩 실패", e);
+        }
+        
+    }
+
+    public String createToken(String userId){
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + TOKEN_VALID_TIME);
+
+        return Jwts.builder()
+        .setSubject(userId)
+        .setIssuedAt(now)
+        .setExpiration(expiry)
+        .signWith(key, SignatureAlgorithm.HS256)
+        .compact();
+    }
+
+    public String getUserIdFromToken(String token){
+        return Jwts.parserBuilder()
+        .setSigningKey(key)
+        .build()
+        .parseClaimsJws(token)
+        .getBody()
+        .getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
