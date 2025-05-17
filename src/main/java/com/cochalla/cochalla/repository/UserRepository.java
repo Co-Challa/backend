24b913537d13e3bbe92e76f093c64fab1677ed76
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-17 14:18:16 +0900

    Create UserRepository.java
    UserRepository 생성

--- src/main/java/com/cochalla/cochalla/repository/UserRepository.java
+++ src/main/java/com/cochalla/cochalla/repository/UserRepository.java
@@ -3,16 +3,19 @@ package com.cochalla.cochalla.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cochalla.cochalla.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUserId(String userId);
+    boolean existsByUserId(String userId);
+    boolean existsByNickname(String nickname);
 
     @Query("SELECT u FROM User u WHERE u.resTime = :hour AND (u.lastSummaryDate < :today OR u.lastSummaryDate IS NULL)")
     List<User> findTargetUsersForSummary(@Param("hour") int hour, @Param("today") LocalDate today);
