d902eb3550893a05c8d8288e3ea897b628bc3fb9
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-17 14:18:16 +0900

    Create UserRepository.java
    UserRepository 생성

---
+++ src/main/java/com/cochalla/cochalla/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.cochalla.cochalla.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cochalla.cochalla.entity.User;
+
+public interface UserRepository extends JpaRepository<User, String>{
+    Optional<User> findByUserId(String userId);
+    boolean existsByUserId(String userId);
+    boolean existsByNickname(String nickname);
+}
