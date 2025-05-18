883cfb22b65d9f2565bc6a763b1a71efd3430dbe
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-18 19:11:36 +0900

    fix. 댓글 조회 시 Pagination 적용을 위해 Pageable 추가

--- src/main/java/com/cochalla/cochalla/repository/CommentRepository.java
+++ src/main/java/com/cochalla/cochalla/repository/CommentRepository.java
@@ -1,7 +1,7 @@
 package com.cochalla.cochalla.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +13,6 @@ public interface CommentRepository extends JpaRepository<Comment, Integer>{
     
     @Query("SELECT new com.cochalla.cochalla.dto.CommentDto(" +
            "c.postCommentId, c.content, c.createdAt, u.userId, u.nickname, u.profileImg) " +
-           "FROM Comment c JOIN c.user u WHERE c.post.postId = :postId ORDER BY c.createdAt ASC")
-    List<CommentDto> findCommentsByPostId(@Param("postId") Integer postId);
+           "FROM Comment c JOIN c.user u WHERE c.post.postId = :postId")
+    Page<CommentDto> findCommentsByPostId(@Param("postId") Integer postId, Pageable pageable);
 }
