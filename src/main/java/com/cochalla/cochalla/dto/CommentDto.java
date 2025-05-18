68a613b2e0f038964e2cd6cdb2d857f3a2494a27
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-18 18:51:23 +0900

    feat. 포스트 상세 페이지로 응답할 DTO 추가

---
+++ src/main/java/com/cochalla/cochalla/dto/CommentDto.java
@@ -0,0 +1,31 @@
+package com.cochalla.cochalla.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CommentDto {
+
+    // Comment
+    Integer commentId;
+    String content;
+    LocalDateTime createdAt;
+    
+    // User
+    String userId;
+    String nickname;
+    Integer profileImg;
+
+    @Override
+    public String toString() {
+        return "\tCommentId = " + commentId
+            + "\n\tUserId = " + userId
+            + "\n\tNickname = " + nickname
+            + "\n\tProfile = " + profileImg
+            + "\n\tContent = " + content
+            + "\n\tCreated = " + createdAt;
+    }
+
+    
+}
