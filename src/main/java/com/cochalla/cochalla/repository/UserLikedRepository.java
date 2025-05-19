d4c546610a13c101cdcba5d17f1f65faf507444f
seoyun <chaeseoyoun8694@gmail.com>
2025-05-19 14:58:24 +0900

    feat. userPost api 좋아요 여부 필드 추가 #3

--- src/main/java/com/cochalla/cochalla/repository/UserLikedRepository.java
+++ src/main/java/com/cochalla/cochalla/repository/UserLikedRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserLikedRepository extends JpaRepository<Like, LikeId> {
+    boolean existsByUserUserIdAndPostPostId(String userId, Integer postId);
     Page<Like> findByUserUserId(String userId, Pageable pageable);
 }
