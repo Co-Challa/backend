e5ce8d50d53973350da766cb8a478c7e86991e5b
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-19 11:33:55 +0900

    fix. 댓글 작성 기능을 위한 Setter 추가

--- src/main/java/com/cochalla/cochalla/domain/Comment.java
+++ src/main/java/com/cochalla/cochalla/domain/Comment.java
@@ -2,10 +2,12 @@ package com.cochalla.cochalla.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
 @Getter
 @Table(name = "comment")
 public class Comment {
