8e9bc4b3431d062495c5f14763d19d97cf4fa773
seoyun <chaeseoyoun8694@gmail.com>
2025-05-20 15:59:20 +0900

    feat. Mypage userliked api 수정
    글 작성자의 닉네임/프로필 사진 조회 -> 쿼리문으로 변경

--- src/main/java/com/cochalla/cochalla/repository/UserLikedRepository.java
+++ src/main/java/com/cochalla/cochalla/repository/UserLikedRepository.java
@@ -1,12 +1,26 @@
 package com.cochalla.cochalla.repository;
 
-import com.cochalla.cochalla.domain.Like;
-import com.cochalla.cochalla.domain.LikeId;
+import com.cochalla.cochalla.dto.UserLikeDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.cochalla.cochalla.domain.Like;
+import com.cochalla.cochalla.domain.LikeId;
 
 public interface UserLikedRepository extends JpaRepository<Like, LikeId> {
+
     boolean existsByUserUserIdAndPostPostId(String userId, Integer postId);
-    Page<Like> findByUserUserId(String userId, Pageable pageable);
+
+    @Query("SELECT new com.cochalla.cochalla.dto.UserLikeDto(" +
+           "p.postId, s.title, s.content, s.createdAt, " +
+           "size(p.comments), size(p.likes), p.isPublic, " +
+           "u.nickname, u.profileImg) " +
+           "FROM Like l " +
+           "JOIN l.post p " +
+           "JOIN p.summary s " +
+           "JOIN p.user u " +
+           "WHERE l.user.userId = :userId")
+    Page<UserLikeDto> findUserLikedPosts(@Param("userId") String userId, Pageable pageable);
 }
