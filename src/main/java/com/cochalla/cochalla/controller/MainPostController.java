ba0c53168198e36947c26796833242ede8ad733b
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-22 21:57:22 +0900

    feat. dahee commits

--- src/main/java/com/cochalla/cochalla/controller/MainPostController.java
+++ src/main/java/com/cochalla/cochalla/controller/MainPostController.java
@@ -2,12 +2,14 @@ package com.cochalla.cochalla.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cochalla.cochalla.dto.MainPostDto;
+import com.cochalla.cochalla.security.CustomUserDetail;
 import com.cochalla.cochalla.service.MainPostService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,11 +24,14 @@ public class MainPostController {
     private static final int PAGE_SIZE = 10; // 페이지당 게시글 수
 
     @GetMapping("/list")
-    public List<MainPostDto> getPostsByPage(@RequestParam(defaultValue = "0") int page) {
+    public List<MainPostDto> getPostsByPage(@RequestParam(defaultValue = "0") int page, 
+    @AuthenticationPrincipal CustomUserDetail userDetail
+    ) {
         int offset = page * PAGE_SIZE; 
-        return mainpostService.getPostSummariesByPage(offset, PAGE_SIZE);
+        String currentUserId = (userDetail != null) ? userDetail.getUsername() : null;
+        return mainpostService.getPostSummariesByPage(offset, PAGE_SIZE,currentUserId);
+        
     }
-
 
 
     
