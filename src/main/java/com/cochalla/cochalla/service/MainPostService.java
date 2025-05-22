315895ba01c2d96a5df677d1566172c469a8afdb
seoyoun8694 <chaeseoyoun8694@gmail.com>
2025-05-22 17:22:36 +0900

    chore. 저장

---
+++ src/main/java/com/cochalla/cochalla/service/MainPostService.java
@@ -0,0 +1,10 @@
+package com.cochalla.cochalla.service;
+
+import com.cochalla.cochalla.dto.MainPostDto;
+import java.util.List;
+
+public interface MainPostService {
+    List<MainPostDto> getAllPostSummaries();
+    List<MainPostDto> getPostSummariesByPage(int offset, int limit);
+}
+
