a12d20e54b016ccc073dae70fda82f2774ade6ab
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-22 10:51:10 +0900

    feat. 쓰지 않는 import 삭제
    feat. 쓰지 않는 import 삭제

--- src/main/java/com/cochalla/cochalla/controller/CommentController.java
+++ src/main/java/com/cochalla/cochalla/controller/CommentController.java
@@ -11,11 +11,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cochalla.cochalla.dto.CommentResponseDto;
 import com.cochalla.cochalla.dto.CommentRequestDto;
-import com.cochalla.cochalla.dto.CommentResponseDto;
 import com.cochalla.cochalla.service.CommentServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
